e1ae050d61efa491327a355081c3a622ccaa00be
Minh Phuc <lephuc04b3@gmail.com>
2024-11-25 06:55:34 -0800

    init project

---
+++ src/main/java/com/example/medical_appointment_booking_app/controller/AdminController.java
@@ -0,0 +1,4 @@
+package com.example.medical_appointment_booking_app.controller;
+
+public class AdminController {
+}
---
+++ src/test/java/com/example/medical_appointment_booking_app/MedicalAppointmentBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medical_appointment_booking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
