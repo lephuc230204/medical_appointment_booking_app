adba7a09c99ae36ab7b2f6e300b2f4b6019821c0
Minh Phuc <lephuc04b3@gmail.com>
2024-11-26 11:43:06 +0700

    init project

--- src/main/java/com/example/medical_appointment_booking_app/payload/response/ResponseData.java
+++ src/main/java/com/example/medical_appointment_booking_app/payload/response/ResponseData.java
@@ -1,19 +1,20 @@
 package com.example.medical_appointment_booking_app.payload.response;
 
 import java.io.Serializable;
+import lombok.Getter;
 
-
+@Getter
 public class ResponseData<T> implements Serializable {
     private final int status;
     private final String message;
     private T data;
-    
+    /**GET, POST*/
     public ResponseData(int status, String message, T data) {
         this.status = status;
         this.message = message;
         this.data = data;
     }
-    
+    /**PUT, PATCH, DELETE**/
     public ResponseData(int status, String message) {
         this.status = status;
         this.message = message;
--- src/main/java/com/example/medical_appointment_booking_app/payload/response/ResponseError.java
+++ src/main/java/com/example/medical_appointment_booking_app/payload/response/ResponseError.java
@@ -1,4 +1,8 @@
 package com.example.medical_appointment_booking_app.payload.response;
 
-public class ResponseError {
+public class ResponseError<T> extends ResponseData<T> {
+    public ResponseError(int status, String message) {
+        super(status, message);
+    }
 }
+
