92ea0e3024ac4dafa3faf357644c0d7e3ec9734c
Minh Phuc <lephuc04b3@gmail.com>
2024-12-09 21:15:39 +0700

    init db

--- src/main/java/com/example/medical_appointment_booking_app/config/DataInitializer.java
+++ src/main/java/com/example/medical_appointment_booking_app/config/DataInitializer.java
@@ -14,30 +14,38 @@
 //
 //@Configuration
 //public class DataInitializer {
+//
 //    @Autowired
 //    private UserRepository userRepository;
+//
 //    @Autowired
 //    private RoleRepository roleRepository;
+//
 //    @Autowired
 //    private PasswordEncoder passwordEncoder;
 //
 //    @PostConstruct
 //    public void init() {
-//        Role roleAdmin = roleRepository.findByName("ROLE_ADMIN").orElse(null);
-//        Role roleUser = roleRepository.findByName("ROLE_USER").orElse(null);
+//        // Kiểm tra và lấy role "ROLE_ADMIN" và "ROLE_USER" từ cơ sở dữ liệu
+//        Role roleAdmin = roleRepository.findByName("ROLE_ADMIN").orElseThrow(() -> new RuntimeException("Role ADMIN not found"));
+//        Role roleUser = roleRepository.findByName("ROLE_USER").orElseThrow(() -> new RuntimeException("Role USER not found"));
 //
+//        // Tạo người dùng admin
 //        User admin = User.builder()
 //                .email("admin@example.com")
-//                .password(passwordEncoder.encode("123456"))
+//                .password(passwordEncoder.encode("123456"))  // Mã hóa mật khẩu
 //                .role(roleAdmin)
 //                .username("admin")
-//                .dob(LocalDate.of(1990, 1, 1))
+//                .dob(LocalDate.of(1990, 1, 1))  // Ngày sinh
 //                .phoneNumber("1234567890")
-//                .status(Status.ACTIVE)
+//                .status(Status.ACTIVE)  // Trạng thái là ACTIVE
+//                .setCreatedDate(LocalDate.now())  // Ngày tạo
 //                .build();
+//
+//        // Lưu người dùng admin vào cơ sở dữ liệu
 //        userRepository.save(admin);
 //
-//
+//        // Tạo người dùng thường
 //        User user = User.builder()
 //                .email("user@example.com")
 //                .password(passwordEncoder.encode("123456"))
@@ -45,8 +53,11 @@
 //                .username("user")
 //                .dob(LocalDate.of(2000, 1, 1))
 //                .phoneNumber("0987654321")
-//                .status(Status.ACTIVE)
+//                .status(Status.ACTIVE)  // Trạng thái là ACTIVE
+//                .setCreatedDate(LocalDate.now())
 //                .build();
+//
+//        // Lưu người dùng thường vào cơ sở dữ liệu
 //        userRepository.save(user);
 //    }
 //}
