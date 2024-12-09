8791038aab5e7024b932dd6922e2d4c5994c665a
Minh Phuc <lephuc04b3@gmail.com>
2024-12-09 21:17:26 +0700

    change form login

--- src/main/java/com/example/medical_appointment_booking_app/payload/request/Form/SignInForm.java
+++ src/main/java/com/example/medical_appointment_booking_app/payload/request/Form/SignInForm.java
@@ -8,18 +8,7 @@ import lombok.Data;
 
 @Data
 public class SignInForm {
-
-    @NotBlank(message = "Username must not be blank")
-    private String username;
-
-    @NotBlank(message = "Email must not be blank")
-    @Email(message = "Invalid email format")
     private String email;
-
-    @NotBlank(message = "Password must not be blank")
-    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
-    @NotBlank(message = "Confirm Password must not be blank")
-    private String confirmPassword;
 }
