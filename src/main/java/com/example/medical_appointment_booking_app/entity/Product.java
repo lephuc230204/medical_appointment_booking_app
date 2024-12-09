a66b6690cf861e66c41b247311657ac32cfc255d
Minh Phuc <lephuc04b3@gmail.com>
2024-12-09 21:16:27 +0700

    add anotion builder

--- src/main/java/com/example/medical_appointment_booking_app/entity/Product.java
+++ src/main/java/com/example/medical_appointment_booking_app/entity/Product.java
@@ -2,6 +2,7 @@ package com.example.medical_appointment_booking_app.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "product")
 public class Product {
