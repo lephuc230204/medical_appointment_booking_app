6e8d9bb5fc7a076dce77a28961a2703e437d6387
Minh Phuc <lephuc04b3@gmail.com>
2024-12-10 08:12:00 +0700

    dto for create product

--- src/main/java/com/example/medical_appointment_booking_app/payload/request/Form/ProductForm.java
+++ src/main/java/com/example/medical_appointment_booking_app/payload/request/Form/ProductForm.java
@@ -1,29 +1,27 @@
 package com.example.medical_appointment_booking_app.payload.request.Form;
 
-import com.example.medical_appointment_booking_app.entity.Category;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class ProductForm {
-    @NotBlank(message = "Product name must not be null")
+
+    @NotBlank(message = "Product name must not be blank")
     private String productName;
 
-    @NotBlank(message = "Category must not be null")
-    private Category category;
+    @NotBlank(message = "Category name must not be blank")
+    private String categoryName;
 
-    @NotBlank(message = "Quantity must not be null")
+    @NotNull(message = "Quantity must not be null")
     @Min(value = 1, message = "Quantity must be greater than 0")
     private Long quantity;
 
-    @NotBlank(message = "Price must not be null")
-    @Min(value = 0, message = "Price must be greater than 1")
+    @NotNull(message = "Price must not be null")
+    @Min(value = 0, message = "Price must be greater than or equal to 0")
     private Double price;
 
     @NotBlank(message = "Description must not be null or empty")
     private String description;
-
 }
