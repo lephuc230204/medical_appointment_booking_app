c16b52d7e9e90415ddf3f91759b776813f7b3e97
Minh Phuc <lephuc04b3@gmail.com>
2024-12-10 08:11:35 +0700

    add api create new product

--- src/main/java/com/example/medical_appointment_booking_app/controller/ProductController.java
+++ src/main/java/com/example/medical_appointment_booking_app/controller/ProductController.java
@@ -1,30 +1,33 @@
 package com.example.medical_appointment_booking_app.controller;
 
-import com.example.medical_appointment_booking_app.payload.request.Dto.ProductDto;
 import com.example.medical_appointment_booking_app.payload.request.Form.ProductForm;
 import com.example.medical_appointment_booking_app.payload.response.ResponseData;
+import com.example.medical_appointment_booking_app.payload.response.ResponseError;
 import com.example.medical_appointment_booking_app.service.ProductService;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("api/v1/products")
 @RestController
 public class ProductController {
-    @Autowired
-    private ProductService productService;
 
-    @PostMapping("/")
-    public ResponseEntity<ResponseData<ProductDto>> create(@Valid @RequestBody ProductForm form){
+    private final ProductService productService;
+
+    @PostMapping("")
+    public ResponseEntity<ResponseData<?>> create(@RequestBody @Valid ProductForm form) {
         return ResponseEntity.ok(productService.create(form));
     }
+
+
 }
--- src/main/java/com/example/medical_appointment_booking_app/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/medical_appointment_booking_app/service/impl/ProductServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.medical_appointment_booking_app.service.impl;
 
+import com.example.medical_appointment_booking_app.entity.Category;
 import com.example.medical_appointment_booking_app.entity.Product;
 import com.example.medical_appointment_booking_app.payload.request.Dto.ProductDto;
 import com.example.medical_appointment_booking_app.payload.request.Form.ProductForm;
 import com.example.medical_appointment_booking_app.payload.response.ResponseData;
+import com.example.medical_appointment_booking_app.repository.CategoryRepository;
 import com.example.medical_appointment_booking_app.repository.ProductRepository;
+import com.example.medical_appointment_booking_app.service.CategoryService;
 import com.example.medical_appointment_booking_app.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,12 +16,16 @@ import org.springframework.stereotype.Service;
 public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private CategoryService categoryService;
 
     @Override
     public ResponseData<ProductDto> create(ProductForm form) {
+        Category category =categoryService.checkOrCreateCategory(form.getCategoryName());
+
         Product product = Product.builder()
                 .productName(form.getProductName())
-                .category(form.getCategory())
+                .category(category)
                 .quantity(form.getQuantity())
                 .currentQuantity(form.getQuantity())
                 .price(form.getPrice())
