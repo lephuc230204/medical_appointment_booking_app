8f72269f7f99f36f5f21d14ac142a25d2006e66b
Minh Phuc <lephuc04b3@gmail.com>
2024-12-10 21:24:18 +0700

    add method find product by id

--- src/main/java/com/example/medical_appointment_booking_app/repository/ProductRepository.java
+++ src/main/java/com/example/medical_appointment_booking_app/repository/ProductRepository.java
@@ -4,8 +4,10 @@ import com.example.medical_appointment_booking_app.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Optional<Product> findByProductId(Long productId);
 
 }
