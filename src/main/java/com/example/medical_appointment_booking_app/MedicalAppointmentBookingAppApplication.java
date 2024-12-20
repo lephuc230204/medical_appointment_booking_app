e50f94c44ce2a516ebc39698e065bb6aba274c2e
Minh Phuc <lephuc04b3@gmail.com>
2024-12-20 22:59:36 +0700

    add restemplate

--- src/main/java/com/example/medical_appointment_booking_app/MedicalAppointmentBookingAppApplication.java
+++ src/main/java/com/example/medical_appointment_booking_app/MedicalAppointmentBookingAppApplication.java
@@ -2,10 +2,17 @@ package com.example.medical_appointment_booking_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MedicalAppointmentBookingAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(MedicalAppointmentBookingAppApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
