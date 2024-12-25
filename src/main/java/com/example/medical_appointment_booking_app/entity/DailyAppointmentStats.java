c64c9aa105a9e6064091152031721a084c4c5a9e
toan <toanthanhnguyen2302@gmail.com>
2024-12-25 19:31:51 +0700

    add appointmentNumber

---
+++ src/main/java/com/example/medical_appointment_booking_app/entity/DailyAppointmentStats.java
@@ -0,0 +1,34 @@
+package com.example.medical_appointment_booking_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class DailyAppointmentStats {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private Long totalAppointments;
+
+    @Column(nullable = false)
+    private Long completedAppointments;
+
+    @Column(nullable = false)
+    private Long cancelledAppointments;
+
+}
--- src/main/java/com/example/medical_appointment_booking_app/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/example/medical_appointment_booking_app/service/impl/AppointmentServiceImpl.java
@@ -1,12 +1,14 @@
 package com.example.medical_appointment_booking_app.service.impl;
 
 import com.example.medical_appointment_booking_app.entity.Appointment;
+import com.example.medical_appointment_booking_app.entity.DailyAppointmentStats;
 import com.example.medical_appointment_booking_app.entity.Schedule;
 import com.example.medical_appointment_booking_app.entity.User;
 import com.example.medical_appointment_booking_app.payload.request.Dto.AppointmentDto;
 import com.example.medical_appointment_booking_app.payload.request.Form.AppointmentForm;
 import com.example.medical_appointment_booking_app.payload.response.ResponseData;
 import com.example.medical_appointment_booking_app.repository.AppointmentRepository;
+import com.example.medical_appointment_booking_app.repository.DailyAppointmentStatsRepository;
 import com.example.medical_appointment_booking_app.repository.ScheduleRepository;
 import com.example.medical_appointment_booking_app.repository.UserRepository;
 import com.example.medical_appointment_booking_app.service.AppointmentService;
@@ -26,6 +28,8 @@ public class AppointmentServiceImpl implements AppointmentService {
     private UserRepository userRepository;
     @Autowired
     private ScheduleRepository scheduleRepository;
+    @Autowired
+    private DailyAppointmentStatsRepository dailyAppointmentStatsRepository;
 
     @Override
     public ResponseData<AppointmentDto> createRequest(AppointmentForm form, Principal principal) {
@@ -40,6 +44,9 @@ public class AppointmentServiceImpl implements AppointmentService {
         if (schedule.isFull()) {
             return new ResponseData<>(400, "List of appointments is full");
         }
+        log.info(schedule.getTimeSchedule().getAppointmentDate().toString());
+        DailyAppointmentStats dailyAppointmentStats = dailyAppointmentStatsRepository.findByDate(schedule.getTimeSchedule().getAppointmentDate())
+                .orElseThrow(() -> new RuntimeException("DailyAppointmentStats not found"));
 
         Appointment appointment = new Appointment();
         appointment.setUser(user);
@@ -49,6 +56,7 @@ public class AppointmentServiceImpl implements AppointmentService {
         appointment.setCreateDate(LocalDate.now());
         appointment.setStatus(Appointment.Status.PENDING);
         appointment.setCome(false);
+        appointment.setAppointmentNumber(dailyAppointmentStats.getTotalAppointments()+1);
         appointmentRepository.save(appointment);
 
         listAppointments.add(appointment);
@@ -57,6 +65,9 @@ public class AppointmentServiceImpl implements AppointmentService {
 
         schedule.updateStatus();
         scheduleRepository.save(schedule);
+
+        dailyAppointmentStats.setTotalAppointments(dailyAppointmentStats.getTotalAppointments()+1);
+        dailyAppointmentStatsRepository.save(dailyAppointmentStats);
 
         return new ResponseData<>(200,"appointment created", AppointmentDto.toDto(appointment));
     }
