56d0a69dd3f4443e4d6d617fb6458f95c69fcbdd
Minh Phuc <lephuc04b3@gmail.com>
2025-01-03 14:20:57 +0700

    add query

--- src/main/java/com/example/medical_appointment_booking_app/repository/ProductRepository.java
+++ src/main/java/com/example/medical_appointment_booking_app/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.medical_appointment_booking_app.repository;
 
 import com.example.medical_appointment_booking_app.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,9 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByProductId(Long productId);
+    @Query("SELECT p FROM Product p" +
+            " WHERE p.productName = :productName" +
+            " AND p.category.categoryName = :categoryName")
+    Optional<Product> findByProductNameAndCategoryName(@Param("productName") String productName, @Param("categoryName") String categoryName);
 
 }
