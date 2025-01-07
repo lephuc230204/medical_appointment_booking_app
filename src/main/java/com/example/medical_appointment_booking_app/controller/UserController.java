e6609564577f7e72e7d7a203853a5fb8e6f966df
Minh Phuc <lephuc04b3@gmail.com>
2025-01-07 22:10:31 +0700

    add function get-user

--- src/main/java/com/example/medical_appointment_booking_app/controller/UserController.java
+++ src/main/java/com/example/medical_appointment_booking_app/controller/UserController.java
@@ -1,27 +1,35 @@
 package com.example.medical_appointment_booking_app.controller;
 
-import com.example.medical_appointment_booking_app.payload.request.Dto.UserDto;
-import com.example.medical_appointment_booking_app.payload.request.Form.UserForm;
+import com.example.medical_appointment_booking_app.payload.request.Dto.UserBasicDto;
+import com.example.medical_appointment_booking_app.payload.response.ResponseData;
+import com.example.medical_appointment_booking_app.payload.response.ResponseError;
 import com.example.medical_appointment_booking_app.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag( name = "UserController", description = "API user quản lí thông tin ca nhan cua minh")
+@Slf4j
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-//    @Operation( summary = "bác sĩ chọn ca làm việc", description = "API cho phép bác sĩ chọn ca làm việc")
-//    @GetMapping("")
-//    public ResponseEntity<Page<UserDto>> getUsers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
-//        return ResponseEntity.ok(userService.getUsers(page,size));
-//    }
-//    @PostMapping("")
-//    public ResponseEntity<UserDto> createUser(@RequestBody UserForm form){
-//        return ResponseEntity.ok(userService.createUser(form));
-//    }
+    @Operation(summary = "Xem chi tiet tai khoan", description = "User co the xem chi tiet account cua minh")
+    @GetMapping("/me")
+    public ResponseEntity<ResponseData<UserBasicDto>> getMyInfo(){
+        log.info("getMyInfo");
+        try{
+            userService.getMyInfo();
+            return ResponseEntity.ok(userService.getMyInfo());
+        }catch (Exception e){
+            log.error("errorMessage={}", e.getMessage(), e.getCause());
+            return ResponseEntity.badRequest().body(new ResponseError<>(400,"Get info was failed"));
+        }
+
+    }
 }
