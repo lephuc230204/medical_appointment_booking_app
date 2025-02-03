5de4b17575975db4a2e8f86fa576caed89789f17
Minh Phuc <lephuc04b3@gmail.com>
2025-02-03 19:24:27 +0700

    update: handle exeption for momo failed

---
+++ src/main/java/com/example/medical_appointment_booking_app/exception/MomoExeption.java
@@ -0,0 +1,7 @@
+package com.example.medical_appointment_booking_app.exception;
+
+public class MomoExeption extends RuntimeException{
+    public MomoExeption(String message) {
+        super(message);
+    }
+}
